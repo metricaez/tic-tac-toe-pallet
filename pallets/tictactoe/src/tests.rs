20d17f7757788b95dbf40378f097bcd01b61be98
metricaez <emiliano@parity.io>
2023-06-30 13:57:02 -0300

    clean up tests

--- pallets/tictactoe/src/tests.rs
+++ pallets/tictactoe/src/tests.rs
@@ -6,7 +6,7 @@ fn initial_state() {
 	new_test_ext().execute_with(|| {
 		assert_eq!(Balances::free_balance(Tictactoe::account_id()), 0);
 		assert_eq!(Tictactoe::game_index(), 0);
-		assert!(crate::Games::<Test>::get(0).is_none());
+		assert!(Tictactoe::games(0).is_none());
 	});
 }
 
@@ -19,10 +19,10 @@ fn create_game_works() {
 		assert_ok!(Tictactoe::start_game(RuntimeOrigin::signed(creator), bet));
 		assert_eq!(Balances::free_balance(&creator), initial_balance - bet);
 		assert_eq!(Tictactoe::game_index(), 1);
-		assert_eq!(crate::Games::<Test>::get(0).unwrap().bet, Some(bet));
-		assert_eq!(crate::Games::<Test>::get(0).unwrap().jackpot, Some(bet));
-		assert_eq!(crate::Games::<Test>::get(0).unwrap().payout_addresses, (creator, creator));
-		assert_eq!(crate::Games::<Test>::get(0).unwrap().ended, false);
+		assert_eq!(Tictactoe::games(0).unwrap().bet, Some(bet));
+		assert_eq!(Tictactoe::games(0).unwrap().jackpot, Some(bet));
+		assert_eq!(Tictactoe::games(0).unwrap().payout_addresses, (creator, creator));
+		assert_eq!(Tictactoe::games(0).unwrap().ended, false);
 		assert_eq!(Balances::free_balance(Tictactoe::account_id()), bet)
 	});
 }
@@ -41,7 +41,7 @@ fn create_game_fails_with_zero_bet() {
 		assert!(Tictactoe::start_game(RuntimeOrigin::signed(creator), bet).is_err());
 		assert_eq!(Balances::free_balance(&creator), initial_balance);
 		assert_eq!(Tictactoe::game_index(), 0);
-		assert!(crate::Games::<Test>::get(0).is_none());
+		assert!(Tictactoe::games(0).is_none());
 		assert_eq!(Balances::free_balance(Tictactoe::account_id()), 0);
 	});
 }
@@ -51,25 +51,17 @@ fn join_a_game_works() {
 	new_test_ext().execute_with(|| {
 		let creator = 1;
 		let joiner = 2;
-		let initial_balance = Balances::free_balance(&creator);
 		let bet = 10;
 		assert_ok!(Tictactoe::start_game(RuntimeOrigin::signed(creator), bet));
-		assert_eq!(Balances::free_balance(&creator), initial_balance - bet);
-		assert_eq!(Tictactoe::game_index(), 1);
-		assert_eq!(crate::Games::<Test>::get(0).unwrap().bet, Some(bet));
-		assert_eq!(crate::Games::<Test>::get(0).unwrap().jackpot, Some(bet));
-		assert_eq!(crate::Games::<Test>::get(0).unwrap().payout_addresses, (creator, creator));
-		assert_eq!(crate::Games::<Test>::get(0).unwrap().ended, false);
-		assert_eq!(Balances::free_balance(Tictactoe::account_id()), bet);
 
 		let initial_balance = Balances::free_balance(&joiner);
 		assert_ok!(Tictactoe::join_game(RuntimeOrigin::signed(joiner), 0));
 		assert_eq!(Balances::free_balance(&joiner), initial_balance - bet);
 		assert_eq!(Tictactoe::game_index(), 1);
-		assert_eq!(crate::Games::<Test>::get(0).unwrap().bet, Some(bet));
-		assert_eq!(crate::Games::<Test>::get(0).unwrap().jackpot, Some(bet * 2));
-		assert_eq!(crate::Games::<Test>::get(0).unwrap().payout_addresses, (creator, joiner));
-		assert_eq!(crate::Games::<Test>::get(0).unwrap().ended, false);
+		assert_eq!(Tictactoe::games(0).unwrap().bet, Some(bet));
+		assert_eq!(Tictactoe::games(0).unwrap().jackpot, Some(bet * 2));
+		assert_eq!(Tictactoe::games(0).unwrap().payout_addresses, (creator, joiner));
+		assert_eq!(Tictactoe::games(0).unwrap().ended, false);
 		assert_eq!(Balances::free_balance(Tictactoe::account_id()), bet * 2);
 	});
 }
@@ -99,35 +91,18 @@ fn end_game_works() {
 		));
 		let creator = 1;
 		let joiner = 2;
-		let initial_balance = Balances::free_balance(&creator);
 		let bet = 10;
 		assert_ok!(Tictactoe::start_game(RuntimeOrigin::signed(creator), bet));
-		assert_eq!(Balances::free_balance(&creator), initial_balance - bet);
-		assert_eq!(Tictactoe::game_index(), 1);
-		assert_eq!(Tictactoe::games(0).unwrap().bet, Some(bet));
-		assert_eq!(crate::Games::<Test>::get(0).unwrap().jackpot, Some(bet));
-		assert_eq!(crate::Games::<Test>::get(0).unwrap().payout_addresses, (creator, creator));
-		assert_eq!(crate::Games::<Test>::get(0).unwrap().ended, false);
-		assert_eq!(Balances::free_balance(Tictactoe::account_id()), bet + pallet_funding);
-
-		let initial_balance = Balances::free_balance(&joiner);
 		assert_ok!(Tictactoe::join_game(RuntimeOrigin::signed(joiner), 0));
-		assert_eq!(Balances::free_balance(&joiner), initial_balance - bet);
-		assert_eq!(Tictactoe::game_index(), 1);
-		assert_eq!(crate::Games::<Test>::get(0).unwrap().bet, Some(bet));
-		assert_eq!(crate::Games::<Test>::get(0).unwrap().jackpot, Some(bet * 2));
-		assert_eq!(crate::Games::<Test>::get(0).unwrap().payout_addresses, (creator, joiner));
-		assert_eq!(crate::Games::<Test>::get(0).unwrap().ended, false);
-		assert_eq!(Balances::free_balance(Tictactoe::account_id()), (bet * 2) + pallet_funding);
 
 		let initial_balance = Balances::free_balance(&creator);
 		assert_ok!(Tictactoe::end_game(RuntimeOrigin::signed(creator), 0, creator));
 		assert_eq!(Balances::free_balance(&creator), initial_balance + bet * 2);
 		assert_eq!(Tictactoe::game_index(), 1);
-		assert_eq!(crate::Games::<Test>::get(0).unwrap().bet, Some(bet));
-		assert_eq!(crate::Games::<Test>::get(0).unwrap().jackpot, Some(0));
-		assert_eq!(crate::Games::<Test>::get(0).unwrap().payout_addresses, (creator, joiner));
-		assert_eq!(crate::Games::<Test>::get(0).unwrap().ended, true);
+		assert_eq!(Tictactoe::games(0).unwrap().bet, Some(bet));
+		assert_eq!(Tictactoe::games(0).unwrap().jackpot, Some(0));
+		assert_eq!(Tictactoe::games(0).unwrap().payout_addresses, (creator, joiner));
+		assert_eq!(Tictactoe::games(0).unwrap().ended, true);
 		assert_eq!(Balances::free_balance(Tictactoe::account_id()), pallet_funding);
 	});
 }
