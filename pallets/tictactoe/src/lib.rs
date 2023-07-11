f3e63923be38db2a12e25736f4d54971bc783ba6
metricaez <emiliano@parity.io>
2023-07-11 15:49:33 -0300

    add comments to lib and refactor force end

--- pallets/tictactoe/src/lib.rs
+++ pallets/tictactoe/src/lib.rs
@@ -1,3 +1,5 @@
+
+
 #![cfg_attr(not(feature = "std"), no_std)]
 
 #[cfg(test)]
@@ -34,10 +36,16 @@ type BalanceOf<T> =
 #[derive(
 	Clone, Encode, Decode, Default, PartialEq, Copy, RuntimeDebug, TypeInfo, MaxEncodedLen,
 )]
+
+/// Game struct
 pub struct Game<Balance, AccountId> {
+	// Bet amount to join the game. Jackpot will be 2x bet amount.
 	bet: Balance,
+	// Stores the payout addresses of the host and joiner.
 	payout_addresses: (Option<AccountId>, Option<AccountId>),
+	// Indicates if the game has ended.
 	ended: bool,
+	// Stores the handshake between host and joiner to agree on the winner.
 	handshake: (Option<AccountId>, Option<AccountId>),
 }
 
@@ -105,14 +113,17 @@ pub mod pallet {
 		HandshakeAlreadySet,
 	}
 
+	/// Index to identify each game.
 	#[pallet::storage]
 	#[pallet::getter(fn game_index)]
 	pub(crate) type GameIndex<T> = StorageValue<_, u32, ValueQuery>;
 
+	/// Safeguard deposit value to be used in case of dispute.
 	#[pallet::storage]
 	#[pallet::getter(fn safeguard_deposit)]
 	pub(crate) type SafeguardDeposit<T> = StorageValue<_, BalanceOf<T>, ValueQuery>;
 
+	/// Storage for game instances.
 	#[pallet::storage]
 	#[pallet::getter(fn games)]
 	pub(crate) type Games<T: Config> =
@@ -120,13 +131,22 @@ pub mod pallet {
 
 	#[pallet::call]
 	impl<T: Config> Pallet<T> {
+
+		/// Create a new game.
+		/// The caller will be the host of the game.
+		/// The bet amount will set the value to other user to join the game.
+		/// Bet amount and safeguard deposit will be transferred to the pallet account.
 		#[pallet::call_index(0)]
 		#[pallet::weight(T::WeightInfo::create_game())]
 		pub fn create_game(origin: OriginFor<T>, bet: BalanceOf<T>) -> DispatchResult {
 			let caller = ensure_signed(origin.clone())?;
 			ensure!(!bet.is_zero(), Error::<T>::CantBeZero);
+
+			// Transfer bet amount and safeguard deposit to pallet account to ensure creator account has enough funds.
 			let transfer_amount = bet.saturating_add(Self::safeguard_deposit());
 			T::Currency::transfer(&caller, &Self::account_id(), transfer_amount, KeepAlive)?;
+
+			// Create new game and write to storage
 			let game_index = Self::game_index();
 			let game = Game {
 				bet,
@@ -142,11 +162,15 @@ pub mod pallet {
 			Ok(())
 		}
 
+		/// Join a game by it's index.
+		/// The caller will be the joiner of the game.
+		/// The alredy set bet and safeguard deposit amount will be transferred to the pallet account.
 		#[pallet::call_index(1)]
 		#[pallet::weight(T::WeightInfo::join_game())]
 		pub fn join_game(origin: OriginFor<T>, game_index: u32) -> DispatchResult {
 			let caller = ensure_signed(origin.clone())?;
 
+			// Retrieve game and update payout address if joiner account has enough funds.
 			Games::<T>::try_mutate(game_index, |game| -> DispatchResult {
 				let mut game = game.as_mut().ok_or_else(|| Error::<T>::GameDoesNotExist)?;
 				ensure!(!game.ended, Error::<T>::GameAlreadyEnded);
@@ -162,6 +186,10 @@ pub mod pallet {
 			Ok(())
 		}
 
+		/// End a game by it's index.
+		/// Game ends when both players agree on the winner or when root forces the end of the game.
+		/// Expected to be called by the two players of the game.
+		/// Each caller proposes a winner. If they match jackpot is sent, otherwise mediation is requested.
 		#[pallet::call_index(2)]
 		#[pallet::weight(T::WeightInfo::end_game())]
 		pub fn end_game(
@@ -190,7 +218,6 @@ pub mod pallet {
 			ensure!(winner == host || winner == joiner, Error::<T>::NotAPlayer);
 
 			// Update handshake and avoid writing to storage if already set
-
 			let new_handshake = match Self::update_handshake(
 				game.handshake,
 				caller.clone(),
@@ -204,6 +231,7 @@ pub mod pallet {
 			// Check if both players have agreed on the winner
 			let mut winner_agreed = true;
 			if new_handshake.0 == None || new_handshake.1 == None {
+				// A winner has been proposed, pending for the other player to propose.
 				Self::deposit_event(Event::WinnerProposed {
 					game_index,
 					winner: winner.clone(),
@@ -211,6 +239,7 @@ pub mod pallet {
 				});
 				winner_agreed = false;
 			} else if new_handshake.0 != new_handshake.1 {
+				// Both players have proposed a winner, but they don't match.
 				Self::deposit_event(Event::MediationRequested { game_index, proposer: caller });
 				winner_agreed = false;
 			}
@@ -229,7 +258,6 @@ pub mod pallet {
 				let jackpot = game.bet.saturating_mul(2u32.into());
 				let safeguard_deposit = Self::safeguard_deposit();
 
-				// TBD: BatchTransfer? Better to add logic and add total transfer amount ?
 				let _ = T::Currency::transfer(
 					&Self::account_id(),
 					&host,
@@ -251,6 +279,8 @@ pub mod pallet {
 			Ok(())
 		}
 
+		/// Set the safeguard deposit value.
+		/// Only root can set this value.
 		#[pallet::call_index(3)]
 		#[pallet::weight(T::WeightInfo::set_safeguard_deposit())]
 		pub fn set_safeguard_deposit(
@@ -263,6 +293,11 @@ pub mod pallet {
 			Ok(())
 		}
 
+		/// Force end a game by it's index.
+		/// Only root can force end a game.
+		/// The winner and deposit beneficiary will receive the jackpot and safeguard deposit respectively.
+		/// The game will be marked as ended.
+		/// This function is expected to be called in case of dispute and game logic must be handled off-chain.
 		#[pallet::call_index(4)]
 		#[pallet::weight(T::WeightInfo::force_end_game())]
 		pub fn force_end_game(
@@ -272,32 +307,35 @@ pub mod pallet {
 			deposit_benefiicary: T::AccountId,
 		) -> DispatchResult {
 			ensure_root(origin)?;
-			let game = match Self::games(game_index) {
-				Some(game) => game,
-				None => return Err(Error::<T>::GameDoesNotExist.into()),
-			};
-			ensure!(!game.ended, Error::<T>::GameAlreadyEnded);
+			
+			Games::<T>::try_mutate(game_index, |game| -> DispatchResult {
+				let mut game = game.as_mut().ok_or_else(|| Error::<T>::GameDoesNotExist)?;
+				ensure!(!game.ended, Error::<T>::GameAlreadyEnded);
+				
+				// Update and end game. Set handshake to signal decision.
+				game.ended = true;
+				game.handshake = (Some(winner.clone()), Some(winner.clone()));
 
-			let new_game = Game {
-				bet: game.bet,
-				payout_addresses: game.payout_addresses,
-				ended: true,
-				handshake: (Some(winner.clone()), Some(winner.clone())),
-			};
-			Games::<T>::insert(game_index, new_game);
-			let jackpot = game.bet.saturating_mul(2u32.into());
-			let safeguard_deposit = Self::safeguard_deposit();
-			let _ = T::Currency::transfer(
-				&Self::account_id(),
-				&deposit_benefiicary,
-				safeguard_deposit,
-				KeepAlive,
-			)?;
-			let _ = T::Currency::transfer(&Self::account_id(), &winner, jackpot, KeepAlive)?;
-			Self::deposit_event(Event::GameEnded { game_index, winner, jackpot });
+				// Transfer jackpot and safeguard deposit, bad actor account will not receive the safeguard deposit.
+				let jackpot = game.bet.saturating_mul(2u32.into());
+				let safeguard_deposit = Self::safeguard_deposit();
+				let _ = T::Currency::transfer(
+					&Self::account_id(),
+					&deposit_benefiicary,
+					safeguard_deposit,
+					KeepAlive,
+				)?;
+				let _ = T::Currency::transfer(&Self::account_id(), &winner, jackpot, KeepAlive)?;
+
+				Self::deposit_event(Event::GameEnded { game_index, winner, jackpot });
+				Ok(())
+			})?;
 			Ok(())
 		}
 
+		/// Withdraw funds from the pallet account.
+		/// Only root can withdraw funds.
+		/// Intended to be used to withdraw funds left from slashed accounts.
 		#[pallet::call_index(5)]
 		#[pallet::weight(T::WeightInfo::withdraw_funds())]
 		pub fn withdraw_funds(
@@ -314,17 +352,22 @@ pub mod pallet {
 }
 
 impl<T: Config> Pallet<T> {
+	
+	/// Returns the pallet account id.
+	/// Store in variable to avoid calling the function multiple times.
 	pub fn account_id() -> T::AccountId {
 		T::PalletId::get().into_account_truncating()
 	}
 
+	/// Update handshake and avoid writing to storage if already set
+	/// Host proposed winner is store in handshake.0
+	/// Joiner proposed winner is store in handshake.1
 	fn update_handshake(
 		handshake: (Option<T::AccountId>, Option<T::AccountId>),
 		caller: T::AccountId,
 		host: T::AccountId,
 		winner: T::AccountId,
 	) -> Result<(Option<T::AccountId>, Option<T::AccountId>), DispatchError> {
-		// Update handshake and avoid writing to storage if already set
 		let mut new_handshake = handshake;
 		if caller == host {
 			if new_handshake.0 == None {
